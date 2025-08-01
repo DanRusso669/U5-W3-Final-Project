9c1a5d99d0c47cba96661b3e98a48e2692f9cd58
Dan Russo <rdaniele669@gmail.com>
2025-08-01 09:27:38 +0200

    Initial configuration setup

---
+++ src/main/java/danrusso/U5_W3_Final_Project/U5W3FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package danrusso.U5_W3_Final_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3FinalProjectApplication.class, args);
+	}
+
+}
